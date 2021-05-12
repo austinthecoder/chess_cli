901d224477ec63749fa6c8706fd057fd2e51ce40
Austin Schneider <me@austinschneider.com>
2021-05-11 21:05:16 -0400

    bundle gem chess_cli --exe --mit --test=rspec --ci=github

---
+++ spec/chess_cli_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe ChessCli do
+  it "has a version number" do
+    expect(ChessCli::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
